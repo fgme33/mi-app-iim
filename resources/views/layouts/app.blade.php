ea977302f59c8e1731df9ee3b46d401ad57feb7b
federico miranda <fmiranda@localhost.localdomain>
2026-09-10 22:42:36 -0400

    Arreglando Boton Cancelar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,6 +105,9 @@
 
     <!-- Bootstrap 5 JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- SweetAlert2 (faltaba) -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @stack('scripts')
 </body>
 </html>
